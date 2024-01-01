ae48b4486ae1a6b14dc90ddf7900789b4ee953cb
joelnyongesa <joelnyongesa148@gmail.com>
2024-01-01 21:15:15 +0300

    route: added an endpoint for logging in a registered user, returning a JsON web token"

--- back-end/src/routes/loginRoute.js
+++ back-end/src/routes/loginRoute.js
@@ -10,6 +10,27 @@ export const logInRoute = {
 
         const db = getDbConnection('my-auth-app-db');
 
-        const user = await db.collection('')
+        const user = await db.collection('users').findOne({ email })
+
+        if (!user){
+            return res.sendStatus(401);
+        }
+
+        const { _id: id, passwordHash } = user
+
+        const isCorrect = await bcrypt.compare(password, passwordHash);
+
+        if (isCorrect){
+            jwt.sign({ id, email}, process.env.JWT_SECRET, {
+                expiresIn: "2d",
+            }, (err, token) => {
+                if (err){
+                    res.status(500).json(err);
+                }
+                res.status(200).json({ token })
+            })
+        } else{
+            res.sendStatus(401);
+        }
     }
 }
