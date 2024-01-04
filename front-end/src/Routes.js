9b903c419dac35e6e9532fe8619a167179141192
joelnyongesa <joelnyongesa148@gmail.com>
2024-01-04 18:21:53 +0300

    route: added a sign up route and made some fixes to the log in route

--- front-end/src/Routes.js
+++ front-end/src/Routes.js
@@ -2,15 +2,20 @@ import React from 'react';
 import {  Routes, Route } from 'react-router-dom';
 import { UserInfoPage } from './pages/userInformation';
 import Login from './pages/Login';import PrivateRoute from './auth/PrivateRoute';
+import Signup from './pages/Signup';
 
 export const AppRoutes = () => {
     return (
         <Routes>
-            <Route
+            {/* <Route
                 path="/"
                 element={<PrivateRoute><UserInfoPage /></PrivateRoute>}
-            />
+            /> */}
+            <Route element={<PrivateRoute />}>
+                <Route element={<UserInfoPage />} path='/' />
+            </Route>
             <Route path="/login" element={<Login />} />
+            <Route path='/signup' element={<Signup />} />
         </Routes>
     );
 };
